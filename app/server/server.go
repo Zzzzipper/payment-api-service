b7b6a8b8d32fa72be26a20533e8fd7507fb60594
Eduard Pozdniakov <e.pozdniakov@mapcard.pro>
2022-11-24 23:39:50 +0300

    Fixed failed cvv type: uint32->string

--- app/server/server.go
+++ app/server/server.go
@@ -30,37 +30,6 @@ func New() *Backend {
 	}
 }
 
-// Examples
-/*
-// AddUser adds a user to the in-memory store.
-func (b *Backend) AddUser(ctx context.Context, _ *pbPayment.AddUserRequest) (*pbPayment.User, error) {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	user := &pbPayment.User{
-		Id: uuid.Must(uuid.NewV4()).String(),
-	}
-	b.users = append(b.users, user)
-
-	return user, nil
-}
-
-// ListUsers lists all users in the store.
-func (b *Backend) ListUsers(_ *pbPayment.ListUsersRequest, srv pbPayment.PaymentService_ListUsersServer) error {
-	b.mu.RLock()
-	defer b.mu.RUnlock()
-
-	for _, user := range b.users {
-		err := srv.Send(user)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-*/
-
 func (b *Backend) Block(ctx context.Context, in *pbPayment.BlockRequest) (*pbPayment.BlockHandler, error) {
 	_, ok := GetUserMetadata(ctx)
 	if !ok {
